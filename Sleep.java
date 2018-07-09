a7a63068210ca1af89346c73b5fec29de56bfb0b
Estefania Lopez <32846703+estefania-ai@users.noreply.github.com>
2018-07-09 15:33:22 -0500

    Add files via upload

---
+++ Sleep.java
@@ -0,0 +1,13 @@
+
+public class Sleep{
+  static public boolean sleepIn(boolean weekday, boolean vacation) {
+    //your code here
+  }
+
+  public static void main(String[] args){
+
+    boolean sleep=sleepIn(true,false);
+    System.out.println(sleep);
+
+  }
+}
