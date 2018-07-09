3acaa4dda54b3b5f0a3cf2f3ea86433d16f96e80
Estefania Lopez <32846703+estefania-ai@users.noreply.github.com>
2018-07-09 15:07:40 -0500

    Add files via upload

---
+++ Gradebook.java
@@ -0,0 +1,20 @@
+public class GradeBook{
+  public static void main(String[] args){
+  int grade = 82;
+/* A is a 90-100
+   B is a 80-89
+   C is a 70-79
+   D is a 60-69
+   F is less than 60
+*/
+    //adjust conditionals and print statements to fit the grading scale
+    if (grade >= 0 && grade < 0) {
+      System.out.println("Your grade is an A");
+    }
+    if (grade >= 0 && grade < 0) {
+      System.out.println("Your grade is a B");
+    }
+    // keep adding if statements to acoount for all the grade options
+
+  }
+}
